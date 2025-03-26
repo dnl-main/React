3a8df0a0d1faac68cfe31ddaecb676f1ba2faaed
dnl-main <dnluz01@gmail.com>
2025-03-26 11:47:55 +0800

    notification css

--- frontend/src/admin/notification/Notification.jsx
+++ frontend/src/admin/notification/Notification.jsx
@@ -1,93 +1,68 @@
 import React from 'react';
 import './notification.css';
+// nav
+import { Navbar } from '../navbar/Navbar';
+import Sidebar from '../sidebar/Sidebar';
+import NotificationCard from './notificationComponent/NotificationCard';
+// icons
+import Calendar_Event from '../../assets/icons/Calendar_Event.svg?react';
+import Circle_Primary from '../../assets/icons/Circle_Primary.svg?react';
+import Edit_Pencil_01 from '../../assets/icons/Edit_Pencil_01.svg?react';
 
 const Notification = () => {
   return (
     <div className="notification">
-      <div className="notification-container">
-        <div className="notification-header">
-          <img src="" className="" alt="notification icon" />
-          <p className="notification-header-text">Notifications</p>
-        </div>
-        <div className="notification-tabs">
-          <div className="notification-tabs-all">
-            <img src="" className="" alt="all icon" />
-            <p className="notification-tabs-all-text">All</p>         
-          </div>
-          <div className="notification-tabs-rescheduled">
-            <img src="" className="" alt="rescheduled icon" />
-            <p className="notification-tabs-rescheduled-text">Rescheduled</p>
-          </div>
-          <div className="notification-tabs-canceled">
-            <img src="" className="" alt="canceled icon" />
-            <p className="notification-tabs-canceled-text">Canceled</p>
-          </div>
-          <div className="notification-tabs-uploaded">
-            <img src="" className="" alt="uploaded icon" />
-            <p className="notification-tabs-uploaded-text">Uploaded</p>
-          </div>         
-        </div>
-         
-        <p className="notification-recent">Recent</p>
-        <div className="notification-card">
+    <Navbar />
+    <Sidebar />
+    <div className="notification-box">
+      <main className="notification-box-in">
+        <header className="notification-header">
+          <Calendar_Event 
+            style={{ 
+              color: "var(--black-color)", 
+              width: "32px", 
+              height: "32px", 
+              "--stroke-width": "4px" 
+            }} 
+          />
+          <p>Notifications</p> 
+        </header> {/* notification-header */}
 
-          <div className="notification-card-indicator">
+        <section className="notification-tabs">
+          <button className="notification-tabs-all">
+            <Circle_Primary style={{ color: "var(--white-color)", width: "20px", height: "20px" }} />
+            <p>All</p>
+          </button> {/* notification-tabs-all */}
 
-          </div>
+          <button className="notification-tabs-rescheduled">
+            <Circle_Primary style={{ color: "var(--primary-color)", width: "20px", height: "20px" }} />
+            <p>Rescheduled</p>
+          </button> {/* notification-tabs-rescheduled */}
 
-          <div className="notification-card-details">
-            <div className="notification-card-details-left">    
-              <img src="" className="" alt="profile icon" />
-            </div>
-            <div className="notification-card-details-right">
-              <div className="notification-card-details-right-top">
-                <p className="notification-card-details-right-top-name">John Doe</p>
-                <p className="notification-card-details-right-top-time">4 hours ago</p>
-                <p className="notification-card-details-right-top-position">Fleet Crew Manager</p>
-              </div>
-              <div className="notification-card-details-right-middle">
-                <p className="notification-card-details-right-middle-description">Rescheduled an appointment for</p>
-                <p className="notification-card-details-right-middle-name">Juan Dela R. Cruz</p>
-              </div>
-              <div className="notification-card-details-right-bottom">
-                <div className="notification-card-details-right-bottom-left">
-                  <img src="" className="" alt="calendar icon" />
-                  <div className="notification-card-details-right-bottom-left-text">
-                    <p className="notification-card-details-right-bottom-left-text-day">Monday</p>
-                    <p className="notification-card-details-right-bottom-left-text-date">DEC - 24 - 2024</p>
-                  </div>
-                </div>
-                <div className="notification-card-details-right-bottom-middle">
-                  <img src="" className="" alt="timer icon" />
-                  <div className="notification-card-details-right-bottom-middle-text">
-                    <p className="notification-card-details-right-bottom-middle-text-starts">Starts at</p>
-                    <p className="notification-card-details-right-bottom-middle-text-time">11:30 AM</p>
-                  </div>
-                </div>
-                <div className="notification-card-details-right-bottom-right">
-                  <img src="" className="" alt="timer icon" />
-                  <div className="notification-card-details-right-bottom-right-text">
-                    <p className="notification-card-details-right-bottom-right-text-starts">End at</p>
-                    <p className="notification-card-details-right-bottom-right-text-time">12:00 PM</p>
-                  </div>           
-                </div>
-              </div>
-            </div>
-          </div>
+          <button className="notification-tabs-canceled">
+            <Circle_Primary style={{ color: "var(--primary-color)", width: "20px", height: "20px" }} />
+            <p>Canceled</p>
+          </button> {/* notification-tabs-canceled */}
 
-          <div className="notification-card-class">
-            <img src="" className="" alt="class icon" />
-            <p className="notification-card-class-text">Trainee 4th Engineer</p>
-          </div>
+          <button className="notification-tabs-uploaded">
+            <Circle_Primary style={{ color: "var(--primary-color)", width: "20px", height: "20px" }} />
+            <p>Uploaded</p>
+          </button> {/* notification-tabs-uploaded */}
+        </section> {/* notification-tabs */}
 
-          <div className="notification-card-button">
-            <div className="notification-card-button-bg">
-              <img src="" className="" alt="edit icon" />
-            </div>
-          </div>
+        <section className="notification-container">
+          <header className="notification-header-recents">
+            <p>Recents</p>
+          </header> {/* notification-header-recents */}
 
-        </div>
-      </div>
+          <div className="notification-cards">
+            <NotificationCard />
+            <NotificationCard />
+            <NotificationCard /> 
+          </div> {/* notification-cards */}
+        </section>
+      </main> {/* notification-box-in */}
+    </div> {/* notification-box */}
     </div>
   );
 };
---
+++ frontend/src/admin/notification/notificationComponent/NotificationCard.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import './notificationCard.css';
+
+import Calendar_Event from '../../../assets/icons/Calendar_Event.svg?react';
+import Circle_Primary from '../../../assets/icons/Circle_Primary.svg?react';
+import Edit_Pencil_01 from '../../../assets/icons/Edit_Pencil_01.svg?react';
+
+const NotificationCard = () => {
+  return (
+					<main className="notification-cards-card">
+            <section className="notification-cards-card-indicator">
+            </section> {/* notification-cards-card-indicator */}
+
+            <section className="notification-cards-card-info">
+              <Circle_Primary style={{ color: "var(--black-color)", width: "72px", height: "72px" }} />
+              <div className="notification-cards-card-info-details">
+                <div className="notification-cards-card-info-details-main">
+                  <p className="notification-cards-card-info-details-main-name">John Daniel R. Doe</p>
+                  <Circle_Primary style={{ color: "var(--black-color-opacity-45)", width: "8px", height: "8px" }} />
+                  <p className="notification-cards-card-info-details-main-time">4 hours ago  - </p>
+                  <p className="notification-cards-card-info-details-main-role">Fleet crew manager</p>
+                </div> {/* notification-cards-card-info-details-main */}
+
+                <div className="notification-cards-card-info-details-sub">
+                  <p className="notification-cards-card-info-details-sub-detail">Rescheduled an appointment for</p>
+                  <p className="notification-cards-card-info-details-sub-name">Juan Dela R. Cruz</p>
+                </div> {/* notification-cards-card-info-details-sub */}
+
+                <div className="notification-cards-card-info-details-box">
+                  <div className="notification-cards-card-info-details-box-date">
+                    <Calendar_Event 
+                      style={{ 
+                        color: "var(--black-color-opacity-60)", 
+                        width: "24px", 
+                        height: "24px", 
+                        "--stroke-width": "4px" 
+                      }} 
+                    />
+                    <div className="notification-cards-card-info-details-box-date-day">
+                      <p className="notification-cards-card-info-details-box-date-day-day">Monday</p>
+                      <p className="notification-cards-card-info-details-box-date-day-date">DEC-28-2024</p>
+                    </div> {/* notification-cards-card-info-details-box-date-day */}
+                  </div> {/* notification-cards-card-info-details-box-date */}
+
+                  <div className="notification-cards-card-info-details-box-start">
+                    <Calendar_Event 
+                      style={{ 
+                        color: "var(--black-color-opacity-60)", 
+                        width: "24px", 
+                        height: "24px", 
+                        "--stroke-width": "4px" 
+                      }} 
+                    />
+                    <div className="notification-cards-card-info-details-box-start-time">
+                      <p className="notification-cards-card-info-details-box-start-time-text">Starts at</p>
+                      <p className="notification-cards-card-info-details-box-start-time-date">11:30 AM</p>
+                    </div> {/* notification-cards-card-info-details-box-start-time */}
+                  </div> {/* notification-cards-card-info-details-box-start */}
+
+                  <div className="notification-cards-card-info-details-box-end">
+                    <Calendar_Event 
+                      style={{ 
+                        color: "var(--black-color-opacity-60)", 
+                        width: "24px", 
+                        height: "24px", 
+                        "--stroke-width": "4px" 
+                      }} 
+                    />
+                    <div className="notification-cards-card-info-details-box-end-time">
+                      <p className="notification-cards-card-info-details-box-end-time-text">Ends at</p>
+                      <p className="notification-cards-card-info-details-box-end-time-date">12:00 PM</p>
+                    </div> {/* notification-cards-card-info-details-box-end-time */}
+                  </div> {/* notification-cards-card-info-details-box-end */}
+                </div> {/* notification-cards-card-info-details-box */}
+              </div> {/* notification-cards-card-info-details */}
+            </section> {/* notification-cards-card-info */}
+
+            <section className="notification-cards-card-role">
+              <div className="notification-cards-card-role-title">
+                <Circle_Primary style={{ color: "var(--primary-color)", width: "32px", height: "32px" }} />
+                <p>Chief Engineer</p>
+              </div> {/* notification-cards-card-role-title */}
+            </section> {/* notification-cards-card-role */}
+
+            <section className="notification-cards-card-button">
+              <button>
+                <Edit_Pencil_01 style={{ color: "var(--white-color)", width: "32px", height: "32px", "--stroke-width": "2px" }} />
+              </button>
+            </section> {/* notification-cards-card-button*/}
+          </main> 
+  );
+};
+
+export default NotificationCard;
